d425e18468803a5a4b0621b6c30289c0282f54d5
Jack Standbridge <jack.s.standbridge@gmail.com>
2021-03-22 12:42:47 +0000

    build project

---
+++ precache-manifest.6190324fa062f478d7870856788db86d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c86416ae5af2673ed7fbc1563c5ab96",
+    "url": "/css-playground/index.html"
+  },
+  {
+    "revision": "2a2c19c7c8f8ac8edbd2",
+    "url": "/css-playground/static/css/main.933000cd.chunk.css"
+  },
+  {
+    "revision": "fa4c13621ef640344b41",
+    "url": "/css-playground/static/js/2.b452c91c.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/css-playground/static/js/2.b452c91c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a2c19c7c8f8ac8edbd2",
+    "url": "/css-playground/static/js/main.53e8e196.chunk.js"
+  },
+  {
+    "revision": "5fb5e7e71fd0f93170ef",
+    "url": "/css-playground/static/js/runtime-main.0a57c7af.js"
+  },
+  {
+    "revision": "e5826e9c7ea681de9beff13e05a89f23",
+    "url": "/css-playground/static/media/flex.e5826e9c.ico"
+  },
+  {
+    "revision": "8a2b2d7c68c431ac1e04554ad0b72c56",
+    "url": "/css-playground/static/media/grid.8a2b2d7c.ico"
+  }
+]);
